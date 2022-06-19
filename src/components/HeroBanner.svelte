c1bac02498b68f660581f1e333a174626bb287d3
Daniel Trochez <daniel@queenslab.se>
2022-06-19 19:48:13 +0200

    new clean intall of svelte

--- src/components/HeroBanner.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import HeroImage from "../../static/assets/verdi-hero-logo_auto_x2.jpg"
-</script>
-
-<div class="w-full">
-    <img src="{HeroImage}" alt="hero-banner" class="object-fill w-full h-full"/>
-</div>
--- src/components/UI/Social.svelte
+++
@@ -1,7 +0,0 @@
-<div class="flex flex-col items-center w-full md:w-1/4 mt-6 md:mt-0">
-	<h4 class="inline-block py-2 px-3 uppercase font-bold tracking-wide text-white-900">Följ oss!</h4>
-	<div class="flex pl-0 md:pl-4 justify-start">
-		<a class="flex items-center hover:text-black mr-6 no-underline" target="_blank" href="https://www.facebook.com/VerdiProducts"><i
-			class="fab fa-facebook-f"></i></a>
-	</div>
-</div>
